28bc5a79da5b98788298d1a044ac95cd9f985d93
Zhurbin <zhurbin.alexander@gmail.com>
2021-12-14 12:40:17 +0300

    Remove lib from gitignore

---
+++ lib/cjs/Calendar/View.d.ts
@@ -0,0 +1,6 @@
+import { RsRefForwardingComponent, WithAsProps } from '../@types/common';
+export interface ViewProps extends WithAsProps {
+    previewOnly?: boolean;
+}
+declare const View: RsRefForwardingComponent<'div', ViewProps>;
+export default View;
